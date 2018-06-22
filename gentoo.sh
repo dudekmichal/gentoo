72e177c66dd306e758eac199348df27f2b08a709
Michal Dudek <michal.dudek1995@gmail.com>
2018-06-22 11:32:56 +0200

    Added packages to install

--- gentoo.sh
+++ gentoo.sh
@@ -29,15 +29,15 @@ install_packages()
   sudo emerge --update --deep \
   x11-base/xorg-server x11-apps/xinit x11-wm/i3-gaps x11-misc/rofi \
   x11-misc/i3status x11-misc/i3lock media-gfx/scrot lxde-base/lxrandr \
-  x11-misc/redshift app-editors/vim net-misc/youtube-dl net-p2p/rtorrent \
+  x11-misc/redshift app-editors/vim app-vim/jedi net-misc/youtube-dl net-p2p/rtorrent \
   app-text/zathura app-misc/mc x11-terms/xterm www-client/links \
-  sys-libs/ncurses media-sound/mpd media-sound/ncmpcpp media-sound/mpc \
+  sys-libs/ncurses media-sound/mpd media-sound/ncmpcpp media-sound/mpc media-video/vlc \
   media-gfx/feh media-plugins/alsa-plugins media-sound/alsamixer-app media-libs/alsa-lib x11-apps/xbacklight \
   dev-util/cmake net-misc/curl net-misc/dhcpcd sys-apps/dbus dev-lang/lua media-sound/gmtp \
-  app-misc/neofetch app-arch/p7zip app-arch/unrar app-portage/genlop \
-  app-arch/unzip dev-lang/python sys-devel/gcc dev-vcs/git virtual/ssh \
+  app-misc/neofetch app-arch/p7zip app-arch/unrar app-portage/genlop app-editors/leafpad \
+  app-arch/unzip dev-lang/python sys-devel/gcc dev-vcs/git net-misc/openssh \
   media-fonts/terminus-font app-shells/zsh sys-power/acpi sys-process/htop \
-  sys-fs/ntfs3g games-roguelike/nethack www-client/firefox
+  x11-apps/xbacklight sys-fs/ntfs3g games-roguelike/nethack www-client/firefox
 
   # When the installation is finished, some environment variables will need to re-initialized before continuing. Source the profile with this command.
   sudo env-update
@@ -49,6 +49,7 @@ config_other()
   rc-update add dhcpcd default
   rc-update add wpa_supplicant default
   rc-update add wicd default
+  rc-update add sshd default
 
   chsh -s /bin/zsh $USER
   xrdb -merge $HOME/.Xresources
